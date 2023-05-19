59418753939724bd6d80cec6f716fa45f9bf025c
Corbin <corbindbullard@gmail.com>
2023-05-19 17:18:43 -0400

    fixed-manage-page-css

--- frontend/src/components/ManageListings/index.js
+++ frontend/src/components/ManageListings/index.js
@@ -16,10 +16,12 @@ const ManageListings = () => {
     if (!items.length) return null
 
     return (
-        <div id='home-page-container'>
-            {items.map((item) => (
-                <ItemCard key={item.id} item={item} />
-            ))}
+        <div id="home-page-container">
+            <div id='home-page-all-items'>
+                {items.map((item) => (
+                    <ItemCard key={item.id} item={item} />
+                ))}
+            </div>
         </div>
     )
 }
