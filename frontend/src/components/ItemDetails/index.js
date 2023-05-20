954fe482b848e177c03c51a5e40f306fec5055a7
Corbin <corbindbullard@gmail.com>
2023-05-20 17:13:12 -0400

    cycle-buttons-work

--- frontend/src/components/ItemDetails/index.js
+++ frontend/src/components/ItemDetails/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { deleteItemThunk, fetchSingleItemThunk } from "../../store/item";
 import { useHistory, useParams } from "react-router";
@@ -11,8 +11,10 @@ const ItemDetails = () => {
     const { itemId } = useParams();
     const history = useHistory();
     const item = useSelector(state => state.items.currentItem);
-    const user = useSelector(state => state.session.user)
-    const itemImages = item?.ItemImages;
+    const user = useSelector(state => state.session.user);
+
+    const [imageIndex, setImageIndex] = useState(0)
+
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -25,13 +27,38 @@ const ItemDetails = () => {
         return acc += curr.stars
     }, 0) / item.ProductReviews?.length).toFixed(2) : "No Reviews"
 
-    // console.log(itemImages);
+    const itemImages = [item?.previewImage].concat(item?.ItemImages?.map(image => image.url))
+    console.log(itemImages);
 
+
+    const changeImage = action => {
+        let currIndex = imageIndex;
+        currIndex += action;
+        if (currIndex > itemImages.length - 1) setImageIndex(0);
+        else if (currIndex < 0) setImageIndex(itemImages.length - 1);
+        else setImageIndex(currIndex);
+    }
+
+    console.log(imageIndex);
     return (
         <div id="item-details-page-container-wrapper">
             <div id="item-details-page-container">
                 <div id="item-details-container">
-                    <img id="item-details-image" src={item.previewImage} />
+                    <div id="item-details-image-container">
+                        {itemImages.length > 1 &&
+                            <>
+                                <button
+                                    onClick={() => changeImage(-1)}>
+                                    PREV
+                                </button>
+                                <button
+                                    onClick={() => changeImage(1)}>
+                                    NEXT
+                                </button>
+                            </>
+                        }
+                        <img id="item-details-image" src={itemImages[imageIndex]} />
+                    </div>
                     <div id="item-details-info-container">
                         <h3>{item.User?.firstName} {item.User?.lastName}</h3>
                         <h2>{item.name}</h2>
