656e9e63deaff3eec7ce691691c56a219adc88b5
Corbin <corbindbullard@gmail.com>
2023-06-16 12:29:46 -0400

    linkedin footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -4,9 +4,12 @@ const Footer = () => {
 
     return (
         <footer id="footer-page-container">
-            <div id="footer-github-link">
-                <a href="https://github.com/CorbinBullard/Delay" target="_blank">
+            <div id="footer-links">
+                <a href="https://github.com/CorbinBullard/Delay" target="_blank" className="link">
                     <i className="fab fa-github"> Delay Github</i>
+                </a>
+                <a href="https://www.linkedin.com/in/corbin-b-ab572b98/" target="_blank" className="link">
+                    <i class="fab fa-linkedin"> Corbin Bullard</i>
                 </a>
             </div>
         </footer>
