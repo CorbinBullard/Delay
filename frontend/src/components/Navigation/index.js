1a7302d36f4fcd3a2dfd45b4858c0da5277a05e4
Corbin <corbindbullard@gmail.com>
2023-06-16 11:57:11 -0400

    fix home button

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { NavLink, useHistory } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 import SearchBar from '../SearchBar';
 import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { fetchAllItemsThunk } from '../../store/item';
 
 function Navigation({ isLoaded }) {
     const history = useHistory();
     const sessionUser = useSelector(state => state.session.user);
     const cartLength = Object.values(useSelector(state => state.cart)).length;
     const location = useLocation();
+    const dispatch = useDispatch();
 
     return (
         <div id='navigation-page-container'>
             <ul>
                 <li >
-                    <NavLink id='navigation-home-button' exact to="/"><i className="fab fa-dyalog"></i>elay</NavLink>
+                    <NavLink id='navigation-home-button' exact to="/"
+                    onClick={() => dispatch(fetchAllItemsThunk())}><i className="fab fa-dyalog"></i>elay</NavLink>
                 </li>
                 {isLoaded && (
                     <>
